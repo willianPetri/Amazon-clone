e9fbc7da5b38ba1cdc9f9a38b37e65266737ca3a
willian <wopetri11@gmail.com>
2021-10-27 22:20:06 -0400

    Added Login Page

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,10 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
+import { Link } from 'react-router-dom';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = e => {
+    e.preventDefault();
+  }
+
+  const register = e => {
+    e.preventDefault();
+  }
+  
+
   return (
-    <div>
-      
+    <div className="login">
+      <Link to="/">
+        <img 
+          className="login-logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" 
+          alt="" 
+        />
+      </Link>
+
+      <div className="login-container">
+        <h1>Sign-in</h1>
+
+        <form>
+          <h5>E-mail</h5>
+          <input 
+            type="text" 
+            value={email} 
+            onChange={e => setEmail(e.target.value)} 
+          />
+
+          <h5>Password</h5>
+          <input 
+            type="password" 
+            value={password} 
+            onChange={e => setPassword(e.target.value)} 
+          />
+
+          <button 
+            type='submit' 
+            onClick={signIn} 
+            className='login-signInButton'
+          >
+            Sign In
+          </button>
+        </form>
+
+        <p>
+          By signing-in you agree to Amazon fake clone Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+        </p>
+
+        <button onClick={register} className='login-registerButton'>Create your Amazon Account</button>
+      </div>
     </div>
   )
 }
