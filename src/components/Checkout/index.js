3e03460ed1824462e5fe016187e2b17aee87e776
willian <wopetri11@gmail.com>
2021-10-29 11:16:04 -0400

    Updated Header

--- src/components/Checkout/index.js
+++ src/components/Checkout/index.js
@@ -7,7 +7,7 @@ import { useStateValue } from '../../StateProvider';
 
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
 
   return (
     <div className="checkout">
@@ -18,6 +18,7 @@ function Checkout() {
           alt="" 
         />
         <div className="checkout-title">
+          <h3>Hello, {user?.email}</h3>
           <h2>Your shopping Basket</h2> 
 
           {basket.map(item => (
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -32,7 +32,7 @@ function Header() {
       <div className="header-nav">
         <Link to={!user && '/login'}>
           <div onClick={handleAuthentication} className="header-option">
-            <span className="header-optionLineOne">Hello Guest</span>
+            <span className="header-optionLineOne">Hello {!user ? 'Guest' : user.email}</span>
             <span className="header-optionLineTwo">{user ? 'Sign Out' : 'Sign In'}</span>
           </div>
         </Link>
